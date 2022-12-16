e174095188406785fe068a5abed135990c163ebb
İbrahim Kurular <57796847+ibrahimkurular@users.noreply.github.com>
2022-12-16 17:21:25 +0200

    Update index.php

--- index.php
+++ index.php
@@ -10,31 +10,18 @@
 
 <body>
     <?php
-    echo "<pre>";
-    $planets = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune", "", "", NULL];
-    $map_arr = array_map(function ($a) {
-        global $planets;
-        if ($a == "") {
-            return NULL;
-        } else {
-            return $a++;
-        }
-    }, $planets);
-    $filter_arr = array_filter($map_arr, function ($a) {
-        return $a != NULL ? $a : false;
-    });
-    function rand_Arr($i)
+    function ucgenCiz($sayi)
     {
-        global $filter_arr;
-        $result = array_flip($filter_arr);
-        return array_rand($result, $i);
+        for ($i = 0; $i <= $sayi; $i++) {
+            $a = 0;
+            while ($a <= $i) {
+                $a++;
+                echo $a * 0;
+            }
+            echo "<br>";
+        }
     }
-
-    print_r(rand_Arr(2));
-    print_r(rand_Arr(3));
-    print_r(rand_Arr(2));
-    print_r(rand_Arr(4));
-    print_r(rand_Arr(5));
+    ucgenCiz(15);  
     ?>
 </body>
 
